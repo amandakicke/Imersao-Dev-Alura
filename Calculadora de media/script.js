23f091abd29bdc486a327790f06be7a5460555ca
amandakicke <amandakicke@gmail.com>
2022-03-16 16:05:34 -0300

    Exercicios feitos na Imersao Dev Alura

---
+++ Calculadora de media/script.js
@@ -0,0 +1,14 @@
+var nome = "Amanda"
+
+var notaPrimeiroBimestre = 9.5
+var notaSegundoBimestre = 7.89987
+var notaTerceiroBimestre = 4.87
+var notaQuartoBimestre = 2.897
+var notaFinal = (notaPrimeiroBimestre + notaSegundoBimestre + notaTerceiroBimestre) / 4
+
+var notaFixada = notaFinal.toFixed(1)
+
+console.log("Bem-vindo, " + nome)
+console.log("Sua média é: " + notaFixada)
+
+//variáveis, strings, console.log, toFixed, operações matemáticas, concatenação
